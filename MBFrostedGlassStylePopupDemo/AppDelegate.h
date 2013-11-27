349a0cf5ffecca78926a7d2ea8a29101ebfcb62c
liuzhitao2000 <liuzhitao2000@gmail.com>
2013-11-27 19:36:04 +0800

    Initial Commit

---
+++ MBFrostedGlassStylePopupDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MBFrostedGlassStylePopupDemo
+//
+//  Created by akira on 13-11-27.
+//  Copyright (c) 2013年 akira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MBFrostedGlassStylePopupDemoTests/MBFrostedGlassStylePopupDemoTests.m
@@ -0,0 +1,34 @@
+//
+//  MBFrostedGlassStylePopupDemoTests.m
+//  MBFrostedGlassStylePopupDemoTests
+//
+//  Created by akira on 13-11-27.
+//  Copyright (c) 2013年 akira. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MBFrostedGlassStylePopupDemoTests : XCTestCase
+
+@end
+
+@implementation MBFrostedGlassStylePopupDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
